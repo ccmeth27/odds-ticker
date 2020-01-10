50086b5a098329f721c693a84f34b4f5acecba2a
Connor Meth <connormeth27@gmail.com>
2020-01-10 16:15:05 -0500

    Updates

---
+++ precache-manifest.c72447601ebd7bf462bd83a5bda6d3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b683616eb65b24d147f4ebc3d91ad6e7",
+    "url": "/odds-ticker/index.html"
+  },
+  {
+    "revision": "03079ce75f80e23657d8",
+    "url": "/odds-ticker/static/css/main.ff12fa96.chunk.css"
+  },
+  {
+    "revision": "0295206bafb4ff5176b7",
+    "url": "/odds-ticker/static/js/2.9199c3be.chunk.js"
+  },
+  {
+    "revision": "03079ce75f80e23657d8",
+    "url": "/odds-ticker/static/js/main.2010a9ec.chunk.js"
+  },
+  {
+    "revision": "fd0cd3bf08f86721e416",
+    "url": "/odds-ticker/static/js/runtime-main.c7a0ea53.js"
+  },
+  {
+    "revision": "97465a6ea4fdbe1b6882987012950498",
+    "url": "/odds-ticker/static/media/black-ticker-texture.97465a6e.jpg"
+  }
+]);
